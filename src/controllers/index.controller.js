05c50abb2a230902a838f2ccaf6c034392f63cce
STBenji <stevenbenjumea9@gmail.com>
2023-03-06 19:36:44 -0500

    extension in .js

--- src/controllers/index.controller.js
+++
@@ -1,8 +0,0 @@
-import { pool } from '../db'
-
-export const index = (req, res) => res.json({ message: "welcome to my api" });
-
-export const ping = async (req, res) => {
-    const [result] = await pool.query('SELECT "pong" as result');
-    res.json(result[0]);
-}
--- src/routes/index.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { index , ping }  from '../controllers/index.controller.js'
-
-const router = Router();
-
-router.get("/", index);
-router.get("/ping", ping);
-
-export default router;
